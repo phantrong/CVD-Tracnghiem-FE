29153f1a69ca89929fc7c14f25ece3d609c687d9
phantrong <trong.phan@amela.vn>
2022-07-29 10:56:48 +0700

    fix-deploy

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import 'antd/dist/antd.css';
 import 'styles/index.scss';
 import 'helper/i18n';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from 'reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,7 +13,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+reportWebVitals();
